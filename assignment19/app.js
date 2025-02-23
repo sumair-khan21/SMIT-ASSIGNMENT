f23666b6a3ebcc00657f4d213cd700769b3f1178
SUMAIR KHAN <117274549+sumair-khan21@users.noreply.github.com>
2025-02-24 04:13:44 +0500

    links updated

---
+++ assignment19/app.js
@@ -0,0 +1,31 @@
+let text = document.getElementById("todo-list");
+let ui = document.getElementById("show");
+
+function add() {
+  if (ui.value.trim() === "") return;
+
+  var li = document.createElement("li");
+  li.innerHTML = `${ui.value} 
+        <div>
+            <button class="edit-btn" onclick="edit(this)">Edit</button> 
+            <button onclick="del(this)">Del</button>
+        </div>
+    `;
+
+  text.appendChild(li);
+  ui.value = "";
+}
+
+function edit(button) {
+  let newTodo = prompt(
+    "Edit your todo:",
+    button.parentElement.firstChild.nodeValue.trim()
+  );
+  if (newTodo) {
+    button.parentElement.firstChild.nodeValue = newTodo;
+  }
+}
+
+function del(button) {
+  button.parentElement.remove();
+}
