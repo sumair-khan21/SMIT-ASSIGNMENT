e07305b44328fccbdd4afa0e21545429f9841b83
SUMAIR KHAN <117274549+sumair-khan21@users.noreply.github.com>
2025-03-04 19:58:07 +0500

    updated

--- assignment19/app.js
+++ assignment19/app.js
@@ -1,31 +1,76 @@
 let text = document.getElementById("todo-list");
 let ui = document.getElementById("show");
+let todoData  = JSON.parse(localStorage.getItem('todo')) || [];
 
-function add() {
-  if (ui.value.trim() === "") return;
 
-  var li = document.createElement("li");
-  li.innerHTML = `${ui.value} 
-        <div>
-            <button class="edit-btn" onclick="edit(this)">Edit</button> 
-            <button onclick="del(this)">Del</button>
-        </div>
+render()
+
+function render(){
+  text.innerHTML = "";
+  for(let i = 0; i < todoData.length; i++){
+    text.innerHTML += `
+    <li>
+        ${todoData[i]} 
+        <button class="edit-btn" onclick="edit(${i})">Edit</button>
+        <button class="del" onclick="deleteItem(${i})">del</button></li>
     `;
 
-  text.appendChild(li);
-  ui.value = "";
-}
-
-function edit(button) {
-  let newTodo = prompt(
-    "Edit your todo:",
-    button.parentElement.firstChild.nodeValue.trim()
-  );
-  if (newTodo) {
-    button.parentElement.firstChild.nodeValue = newTodo;
   }
 }
 
-function del(button) {
-  button.parentElement.remove();
+function add() {
+  if (ui.value.trim() !== ""){
+    // var li = document.createElement("li");
+    // // li.innerHTML = `${ui.value} 
+    // //       <div>
+    // //           <button class="edit-btn" onclick="edit(this)">Edit</button> 
+    // //           <button onclick="del(this)">Del</button>
+    // //       </div>
+    // //   `;
+  
+    // text.appendChild(li);
+
+    todoData.push(ui.value)
+    localStorage.setItem('todo', JSON.stringify(todoData))
+    ui.value = "";
+    render()
+  }else{
+   Swal.fire("Please Enter Todo!");
+
+  }
+
+ 
 }
+
+
+function deleteItem(button){
+  todoData.splice(button,1)
+  localStorage.setItem('todo', JSON.stringify(todoData));
+  render()
+}
+
+function edit(button){
+  let updated = prompt("Edited your todo" , todoData[button])
+  if(updated !== "" && updated.trim() !== ""){
+ todoData[button] = updated;
+ localStorage.setItem('todo', JSON.stringify(todoData))
+  }
+  render()
+}
+
+// --------------------------------------------------------
+
+// function edit(button) {
+//   let newTodo = prompt(
+//     "Edit your todo:",
+//     button.parentElement.firstChild.nodeValue.trim()
+//   );
+//   if (newTodo) {
+//     button.parentElement.firstChild.nodeValue = newTodo;
+//   }
+// }
+
+// function del(button) {
+//   button.parentElement.remove();
+// }
+
